0d52f88ad136ced85bd3984389f38f8afd38190d
vangelif <ev.fotiades@yahoo.com>
2023-06-01 19:31:31 +0300

    build loading and error state

--- src/Components/Quotes/quotes.js
+++ src/Components/Quotes/quotes.js
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import './quotes.css';
 
 const Quotes = ({ category }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const API_URL = 'https://api.api-ninjas.com/v1/quotes?category=';
+  const token = 'T1Y9s/5FzZtrHk7QSF4JIQ==7Ld1PDV8j8SRMOxG';
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        `https://api.api-ninjas.com/v1/quotes?category=${category}`,
-        {
+      setLoading(true);
+      try {
+        const response = await fetch(`${API_URL}${category}`, {
           headers: {
-            'X-Api-Key': 'T1Y9s/5FzZtrHk7QSF4JIQ==7Ld1PDV8j8SRMOxG',
+            'X-Api-Key': token,
             'Content-Type': 'application/json',
           },
-        }
-      );
-      const jsonResponse = await response.json();
-      setData(jsonResponse);
-      console.log(jsonResponse);
+        });
+        const jsonResponse = await response.json();
+        setData(jsonResponse);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
     };
     fetchData();
-  }, []);
+  }, [setData]);
 
   return (
-    <div>
+    <div key="quotes" className="quote-wrapper">
       {data.map((item) => (
         <>
-          <h2>{item.author}</h2>
-          <p>{item.quote}</p>
+          <h2 key={item.author}>{item.author}</h2>
+          <p key={Math.trunc(Math.random * 10000)}>{item.quote}</p>
+          <p className="loading">{loading ? 'Loading....' : ''}</p>
+          <p className="error">{error ? 'Houston we have a problem...' : ''}</p>
         </>
       ))}
     </div>
